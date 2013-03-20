d859e08f3eafa500038bb16cb3aa4f91322667d4
mahhek <mahhek@mahhek-Inspiron-N5050.(none)>
2013-03-20 13:42:14 +0500

    updates on sign up process

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,17 +6,19 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
     @user = User.new(params[:user])
-    @user.save
-    if params[:user][:company_id]
-      @user.company_id = params[:user][:company_id]
+    if @user.save
+      if params[:user][:company_id]
+        @user.company_id = params[:user][:company_id]
+      else
+        @company = Company.new(:company_name => params[:company_name], :subdomain => params[:subdomain])
+        @company.save
+        @user.company_id = @company.id
+      end
+      flash[:notice] = "User is created successfully!"
     else
-      @company = Company.new(:company_name => params[:company_name], :subdomain => params[:subdomain])
-      @company.save
-      @user.company_id = @company.id
+      flash[:notice] = "User can't be created!Plese try again or later."
     end
-    @user.save
-#    super
+    redirect_to "/"
+    #    super
   end
-  
- 
 end
