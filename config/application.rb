3fa694e6bd307d1155ebb6beed866377136f3010
mahhek <mahhek@mahhek-Inspiron-N5050.(none)>
2013-03-28 10:03:46 +0500

    updates on layout and registration process

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,7 @@ module Gmaps
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
